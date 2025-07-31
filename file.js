e53b1ad5a44c1a4db1266f6f3b7b49e901691e81
Santosh Adhvaidh <101855566+Blank0210@users.noreply.github.com>
2025-07-31 09:09:01 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,11 +1,33 @@
+// Import the built-in 'fs' module for file system operations
 const fs = require('fs');
 
-// This code writes "Hello, World!" to a file named test.txt 
-// Synchronously
-fs.writeFileSync('./test.txt', 'Hello, World!');
+// Write "Hello, World!" to 'test.txt' synchronously
+// fs.writeFileSync('./test.txt', 'Hello, World!');
 
-// Asynchronously
-fs.writeFile('./test.txt', 'Hello, World! Async', (err) => {});
+// Write "Hello, World! Async" to 'test.txt' asynchronously
+// fs.writeFile('./test.txt', 'Hello, World! Async', (err) => {});
 
-const contact = fs.readFile('./contacts.txt', 'utf8', (err) => {});
-console.log(contact);
+// Read 'contacts.txt' asynchronously and print its contents
+// fs.readFile('./contacts.txt', "utf-8", (err, data) => {
+//     if (err) {
+//         console.error("Error reading file:", err);
+//     } else {
+//         console.log("File read successfully");
+//         console.log(data);
+//     }
+// });
+
+// Copy 'test.txt' to 'test_copy.txt' synchronously
+// fs.cpSync("./test.txt", "./test_copy.txt");
+
+// Delete 'test_copy.txt' synchronously
+// fs.unlinkSync("./test_copy.txt");
+
+// Append text to 'test.txt' synchronously
+// fs.appendFileSync("./test.txt", "\nThis is appended text.");
+
+// Get and print stats (info) about 'test.txt' synchronously
+console.log(fs.statSync("./test.txt"));
+
+// Create a new directory 'new_folder/new' synchronously
+fs.mkdirSync("./new_folder/new");
