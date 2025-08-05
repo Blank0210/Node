22826ffebdc85e8f723a316caaa162ddc7918666
Santosh Adhvaidh <101855566+Blank0210@users.noreply.github.com>
2025-08-05 17:48:58 +0530

    Update index.js

--- REST_API/index.js
+++ REST_API/index.js
@@ -1,30 +1,84 @@
 const express = require('express');
+const fs = require('fs');
+const mongoose = require('mongoose');
+
 const users = require('./MOCK_DATA.json');
+const { type } = require('os');
 
 const PORT = 8000;
 const app = express();
 
+// Connection to MongoDB
+mongoose.connect("")
+
+
+// Schema
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true 
+    },
+    last_name: {
+        type: String,
+        required: true 
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    job_title: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    }
+},{ timestamps: true });
+
+const User = mongoose.model('user', userSchema);
+
+// Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use((req, res, next) => {
+    console.log("Hello from middleware1");
+    next(); 
+});
+
+app.use((req, res, next) => {
+    console.log("Hello from middleware2");
+    next(); 
+});
+
 // ROUTES
 app.get('/users', (req, res) => {
-    const html = `<ul>
+    const html = 
+    `<ul>
         ${users.map(user => `<li>${user.first_name}</li>`).join('')}
-        </ul>`
+    </ul>`
     res.send(html);
 });
 
 // REST API route
 app.get('/api/users', (req, res) => {
+    res.setHeader("X-MyName", "John Doe");
     return res.json(users);
 });
 
-app.route('/api/users/:id').get((req, res) => {
-     const id = Number(req.params.id);
+// Made this because the routes are the same for the get, patch and delete - (/api/users/:id)
+app.route('/api/users/:id')
+.get((req, res) => {
+    const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if ( !user) return res.status(404).json({error: "User not found"});
     return res.json(user);
-}).patch((req, res) => {
+})
+.patch((req, res) => {
     // Edit users
     return res.json({status: "Pending"});
-}).delete((req, res) => {
+})
+.delete((req, res) => {
     // Delete users
     return res.json({status: "Pending"});
 });
