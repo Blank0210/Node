f69adbe649e8d2937d50a3135a1dd4324237d007
Santosh Adhvaidh <101855566+Blank0210@users.noreply.github.com>
2025-08-09 18:25:45 +0530

    Update index.js

--- short_url/index.js
+++ short_url/index.js
@@ -3,6 +3,7 @@ const { connectDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 const path = require("path");
+const staticRoute = require("./routes/staticRouter"); 
 const { render } = require("ejs");
 const app = express();
 const PORT = 8001;
@@ -11,6 +12,7 @@ connectDB("mongodb://localhost:27017/short_url")
 .then( () => console.log("Connected to MongoDB"))
 
 app.use(express.json());
+app.set(express.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -23,6 +25,7 @@ app.get("/test", async (req, res) => {
 });
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get("/:shortId", async(req, res) => {
     const shortId = req.params.shortId;
