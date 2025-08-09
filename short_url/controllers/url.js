8c50bb92d1764c494684c0d1d667b787cd51a741
Santosh Adhvaidh <101855566+Blank0210@users.noreply.github.com>
2025-08-09 17:58:02 +0530

    Create url.js

---
+++ short_url/controllers/url.js
@@ -0,0 +1,33 @@
+const shortid  = require("shortid");
+const URL = require("../models/url");
+
+async function createShortUrl(req, res) {
+    const body = req.body;
+
+    if(!body.url){
+        res.status(400)
+        .json({
+            error: "URL is required"
+        })
+    }
+
+    const shortId = nanoid(8);
+    await URL.create( {
+        shortId: shortID,
+        redirectUrl: body.url,
+        visitHistrory: [],
+    });
+
+    return res.json({ id: shortID })
+}
+
+async function getAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId })
+    return res.json({ totalClicks: result.visitHistrory.length, 
+                      analytics: result.visitHistrory });
+}
+module.exports = {
+    createShortUrl,
+    getAnalytics
+}
